ad096c1d27cc61564eae789441761a37be580ab8
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-23 19:14:57 +0200

    Add Assignment model with validation for classroom assignments

---
+++ KyberKlass.Data.Models/Behavior.cs
@@ -0,0 +1,30 @@
+﻿namespace KyberKlass.Data.Models;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static Common.EntityValidations.Behavior;
+
+/// <summary>
+///     Represents a unacceptable behavior for a student.
+/// </summary>
+public class Behavior
+{
+	[Required]
+	public Guid StudentId { get; set; }
+
+	[ForeignKey(nameof(StudentId))]
+	public Student Student { get; set; } = null!;
+
+	[Required]
+	public Guid ClassroomId { get; set; }
+
+	[ForeignKey(nameof(ClassroomId))]
+	public Classroom Classroom { get; set; } = null!;
+
+	[Required]
+	public DateTime Date { get; set; }
+
+	[Required]
+	[MaxLength(MAX_DESCRIPTION_LENGTH)]
+	public string Description { get; set; } = null!;
+}
