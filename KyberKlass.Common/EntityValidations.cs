1d1ebece58a38dc5f01f7f3980d7a301ccca198e
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-23 16:22:07 +0200

    Add constants for the student

--- KyberKlass.Common/EntityValidations.cs
+++ KyberKlass.Common/EntityValidations.cs
@@ -2,4 +2,18 @@
 
 public static class EntityValidations
 {
+	public static class Person
+	{
+		public const int MIN_NAME_LENGTH = 2;
+		public const int MAX_NAME_LENGTH = 30;
+	}
+
+	public static class Student
+	{
+		public const int MIN_GRADE_LEVEL = 1;
+		public const int MAX_GRADE_LEVEL = 12;
+
+		public const double MIN_GRADE_MARK = 2.00;
+		public const double MAX_GRADE_MARK = 6.00;
+	}
 }
