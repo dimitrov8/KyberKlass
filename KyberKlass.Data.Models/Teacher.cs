b26d51ba8af79636334a21253bbf801af7585898
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-23 16:22:42 +0200

    Define Teacher model

---
+++ KyberKlass.Data.Models/Teacher.cs
@@ -0,0 +1,41 @@
+﻿namespace KyberKlass.Data.Models;
+
+using System.ComponentModel.DataAnnotations;
+using static Common.EntityValidations.Person;
+
+public class Teacher
+{
+	public Teacher()
+	{
+		this.Id = Guid.NewGuid();
+		this.SubjectsTaught = new HashSet<TeacherSubject>();
+	}
+
+	[Key]
+	public Guid Id { get; set; }
+
+	[Required]
+	[MaxLength(MAX_NAME_LENGTH)]
+	public string FirstName { get; set; } = null!;
+
+	[Required]
+	[MaxLength(MAX_NAME_LENGTH)]
+	public string LastName { get; set; } = null!;
+
+	[Required]
+	public DateTime BirthDate { get; set; }
+
+	[Required]
+	public string Address { get; set; } = null!;
+
+	[Required]
+	[EmailAddress]
+	public string Email { get; set; } = null!;
+
+	[Required]
+	[Phone]
+	public string PhoneNumber { get; set; } = null!;
+
+	public ICollection<TeacherSubject> SubjectsTaught { get; set; }
+
+}
