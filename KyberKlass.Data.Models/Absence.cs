3dafe2e8b40f2734a535aae0f450baf14cf9fcc6
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-23 19:13:43 +0200

    Add Absence model representing student absences

---
+++ KyberKlass.Data.Models/Absence.cs
@@ -0,0 +1,22 @@
+﻿namespace KyberKlass.Data.Models;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+/// <summary>
+///     Represents an absence record for a student in the school.
+/// </summary>
+public class Absence
+{
+	[Required]
+	public Guid StudentId { get; set; }
+
+	[ForeignKey(nameof(StudentId))]
+	public Student Student { get; set; } = null!;
+
+	[Required]
+	public DateTime Date { get; set; }
+
+	[Required]
+	public bool IsExcused { get; set; }
+}
