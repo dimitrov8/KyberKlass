9071338defcb657d8bd82bc3b42d749ef8a5a7e4
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-23 16:22:32 +0200

    Define StudentGrade Model

---
+++ KyberKlass.Data.Models/StudentGrade.cs
@@ -0,0 +1,21 @@
+﻿namespace KyberKlass.Data.Models;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static Common.EntityValidations.Student;
+
+public class StudentGrade
+{
+	[Key]
+	public int Id { get; set; }
+
+	[Required]
+	[Range(MIN_GRADE_MARK, MAX_GRADE_MARK)]
+	public double Grade { get; set; }
+
+	[Required]
+	public Guid StudentId { get; set; }
+
+	[ForeignKey(nameof(StudentId))]
+	public Student Student { get; set; } = null!;
+}
