a10cba74e83db7db5016b55a6fccfefe6f9eb285
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-23 19:18:06 +0200

    Implement Classroom model for managing school classrooms

---
+++ KyberKlass.Data.Models/Classroom.cs
@@ -0,0 +1,43 @@
+﻿namespace KyberKlass.Data.Models;
+
+using System.ComponentModel.DataAnnotations;
+using static Common.EntityValidations.Classroom;
+
+/// <summary>
+///     Represents a classroom in the school.
+/// </summary>
+public class Classroom
+{
+	public Classroom()
+	{
+		this.Id = Guid.NewGuid();
+		this.Students = new HashSet<Student>();
+		this.Subjects = new HashSet<Subject>();
+		this.Grades = new HashSet<StudentGrade>();
+		this.Assignments = new HashSet<Assignment>();
+		this.Exams = new HashSet<Exam>();
+		this.Absences = new HashSet<Absence>();
+		this.Praises = new HashSet<Praise>();
+	}
+
+	[Key]
+	public Guid Id { get; set; }
+
+	[Required]
+	[MaxLength(MAX_NAME_LENGTH)]
+	public string Name { get; set; } = null!;
+
+	public ICollection<Student> Students { get; set; }
+
+	public ICollection<Subject> Subjects { get; set; }
+
+	public ICollection<StudentGrade> Grades { get; set; }
+
+	public ICollection<Assignment> Assignments { get; set; }
+
+	public ICollection<Exam> Exams { get; set; }
+
+	public ICollection<Absence> Absences { get; set; }
+
+	public ICollection<Praise> Praises { get; set; }
+}
