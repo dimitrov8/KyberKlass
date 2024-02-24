b5b38f8e20ea59d35af8cb0f6b21c9e737efbd4a
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-02-24 11:34:39 +0200

    Add ClassroomEntityConfiguration class

---
+++ KyberKlass.Data/Configurations/ClassroomEntityConfiguration.cs
@@ -0,0 +1,41 @@
+﻿namespace KyberKlass.Data.Configurations;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Models;
+
+public class ClassroomEntityConfiguration : IEntityTypeConfiguration<Classroom>
+{
+	public void Configure(EntityTypeBuilder<Classroom> builder)
+	{
+		builder
+			.HasMany(c => c.Students)
+			.WithOne(s => s.Classroom)
+			.HasForeignKey(s => s.ClassroomId)
+			.OnDelete(DeleteBehavior.Cascade);
+
+		builder
+			.HasMany(c => c.Absences)
+			.WithOne(a => a.Classroom)
+			.HasForeignKey(a => a.ClassroomId)
+			.OnDelete(DeleteBehavior.Restrict);
+
+		builder
+			.HasMany(c => c.Praises)
+			.WithOne(p => p.Classroom)
+			.HasForeignKey(p => p.ClassroomId)
+			.OnDelete(DeleteBehavior.Restrict);
+
+		builder
+			.HasMany(c => c.Assignments)
+			.WithOne(a => a.Classroom)
+			.HasForeignKey(a => a.ClassroomId)
+			.OnDelete(DeleteBehavior.Restrict);
+
+		builder
+			.HasMany(c => c.Exams)
+			.WithOne(e => e.Classroom)
+			.HasForeignKey(e => e.ClassroomId)
+			.OnDelete(DeleteBehavior.Restrict);
+	}
+}
