9e0d8048f66f51475442438d7573c51d9e8e08e2
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-03-05 16:34:57 +0200

    Create IUserService

---
+++ KyberKlass.Services.Data/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿namespace KyberKlass.Services.Data.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Web.ViewModels.Admin.User;
+
+    public interface IUserService
+    {
+        Task<List<UserViewModel>> AllAsync();
+
+        Task<UserDetailsViewModel?> GetUserDetailsAsync(string id);
+    }
+}
