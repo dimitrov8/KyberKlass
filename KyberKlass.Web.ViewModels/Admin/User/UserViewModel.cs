76870f510e879170c1efa36c68754b68008af540
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-03-05 16:40:57 +0200

    Create UserViewModel

---
+++ KyberKlass.Web.ViewModels/Admin/User/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace KyberKlass.Web.ViewModels.Admin.User;
+
+public class UserViewModel
+{
+    public string Id { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string FullName = null!;
+
+    public string? Role { get; set; }
+
+    public bool IsActive { get; set; }
+}
