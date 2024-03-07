765ec63a5d203cf5e18de327af327482e814e662
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-03-07 23:57:32 +0200

    Rename ValidateInputAsync to IsNotNullOrEmptyInputAsync

--- KyberKlass.Services.Data/SchoolService.cs
+++ KyberKlass.Services.Data/SchoolService.cs
@@ -8,195 +8,192 @@ using Web.ViewModels.Admin.School;
 
 public class SchoolService : ISchoolService
 {
-    private readonly KyberKlassDbContext _dbContext;
+	private readonly KyberKlassDbContext _dbContext;
 
-    public SchoolService(KyberKlassDbContext dbContext)
-    {
-        this._dbContext = dbContext;
-    }
+	public SchoolService(KyberKlassDbContext dbContext)
+	{
+		this._dbContext = dbContext;
+	}
 
-    // Adding school
-    public async Task<bool> AddAsync(AddSchoolFormModel model)
-    {
-        var newSchool = new School
-        {
-            Id = Guid.NewGuid(),
-            Name = model.Name,
-            Address = model.Address,
-            Email = model.Email,
-            PhoneNumber = model.PhoneNumber,
-            IsActive = true
-        };
+	// Adding school
+	public async Task<bool> AddAsync(AddSchoolFormModel model)
+	{
+		var newSchool = new School
+		{
+			Id = Guid.NewGuid(),
+			Name = model.Name,
+			Address = model.Address,
+			Email = model.Email,
+			PhoneNumber = model.PhoneNumber,
+			IsActive = true
+		};
 
-        bool schoolExists = await this.ExistAsync(newSchool);
+		bool schoolExists = await this.ExistAsync(newSchool);
 
-        if (schoolExists)
-        {
-            return false;
-        }
+		if (schoolExists)
+		{
+			return false;
+		}
 
-        await this._dbContext
-            .Schools
-            .AddAsync(newSchool);
+		await this._dbContext
+			.Schools
+			.AddAsync(newSchool);
 
-        await this._dbContext
-            .SaveChangesAsync();
+		await this._dbContext
+			.SaveChangesAsync();
 
-        return true;
-    }
+		return true;
+	}
 
-    // Visualizing schools
-    public async Task<IEnumerable<SchoolViewModel>> AllAsync()
-    {
-        IEnumerable<SchoolViewModel> schools = await this._dbContext
-            .Schools
-            .Select(s => new SchoolViewModel
-            {
-                Id = s.Id.ToString(),
-                Name = s.Name,
-                Address = s.Address,
-                Email = s.Email,
-                PhoneNumber = s.PhoneNumber,
-                IsActive = true
-            })
-            .AsNoTracking()
-            .ToArrayAsync();
+	// Visualizing schools
+	public async Task<IEnumerable<SchoolViewModel>> AllAsync()
+	{
+		IEnumerable<SchoolViewModel> schools = await this._dbContext
+			.Schools
+			.Select(s => new SchoolViewModel
+			{
+				Id = s.Id.ToString(),
+				Name = s.Name,
+				Address = s.Address,
+				Email = s.Email,
+				PhoneNumber = s.PhoneNumber,
+				IsActive = true
+			})
+			.AsNoTracking()
+			.ToArrayAsync();
 
-        return schools;
-    }
+		return schools;
+	}
 
-    // Checks if school exists
-    public async Task<bool> ExistAsync(School newSchool)
-    {
-        return await this._dbContext
-            .Schools
-            .AnyAsync(s => s.Name == newSchool.Name);
-    }
+	// Checks if school exists
+	public async Task<bool> ExistAsync(School newSchool)
+	{
+		return await this._dbContext
+			.Schools
+			.AnyAsync(s => s.Name == newSchool.Name);
+	}
 
-    // Gets school for editing
-    public async Task<AddSchoolFormModel?> GetForEditAsync(string? id)
-    {
-        if (string.IsNullOrEmpty(id) == false)
-        {
-            var viewModel = await this._dbContext
-                .Schools
-                .Where(s => s.Id.ToString() == id)
-                .Select(s => new AddSchoolFormModel
-                {
-                    Name = s.Name,
-                    Address = s.Address,
-                    Email = s.Email,
-                    PhoneNumber = s.PhoneNumber,
-                    IsActive = s.IsActive
-                })
-                .AsNoTracking()
-                .FirstOrDefaultAsync();
+	// Gets school for editing
+	public async Task<AddSchoolFormModel?> GetForEditAsync(string? id)
+	{
+		if (string.IsNullOrEmpty(id) == false)
+		{
+			var viewModel = await this._dbContext
+				.Schools
+				.Where(s => s.Id.ToString() == id)
+				.Select(s => new AddSchoolFormModel
+				{
+					Name = s.Name,
+					Address = s.Address,
+					Email = s.Email,
+					PhoneNumber = s.PhoneNumber,
+					IsActive = s.IsActive
+				})
+				.AsNoTracking()
+				.FirstOrDefaultAsync();
 
-            return viewModel;
-        }
+			return viewModel;
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    public async Task<SchoolViewModel?> GetForDeleteAsync(string? id)
-    {
-        if (string.IsNullOrEmpty(id) == false)
-        {
-            var viewModel = await this._dbContext
-                .Schools
-                .Where(s => s.Id.ToString() == id)
-                .Select(s => new SchoolViewModel
-                {
-                    Id = s.Id.ToString(),
-                    Name = s.Name,
-                    Address = s.Address,
-                    Email = s.Email,
-                    PhoneNumber = s.PhoneNumber,
-                    IsActive = s.IsActive
-                })
-                .AsNoTracking()
-                .FirstOrDefaultAsync();
+	public async Task<SchoolViewModel?> GetForDeleteAsync(string? id)
+	{
+		if (string.IsNullOrEmpty(id) == false)
+		{
+			var viewModel = await this._dbContext
+				.Schools
+				.Where(s => s.Id.ToString() == id)
+				.Select(s => new SchoolViewModel
+				{
+					Id = s.Id.ToString(),
+					Name = s.Name,
+					Address = s.Address,
+					Email = s.Email,
+					PhoneNumber = s.PhoneNumber,
+					IsActive = s.IsActive
+				})
+				.AsNoTracking()
+				.FirstOrDefaultAsync();
 
-            return viewModel;
-        }
+			return viewModel;
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    // Editing school
-    public async Task<bool> EditSchoolAsync(string id, SchoolViewModel model)
-    {
-        var schoolForEdit = await this._dbContext
-            .Schools
-            .FirstOrDefaultAsync(s => s.Id.ToString() == id);
+	// Editing school
+	public async Task<bool> EditSchoolAsync(string id, SchoolViewModel model)
+	{
+		var schoolForEdit = await this._dbContext
+			.Schools
+			.FirstOrDefaultAsync(s => s.Id.ToString() == id);
 
-        if (schoolForEdit == null)
-        {
-            return false;
-        }
+		if (schoolForEdit == null)
+		{
+			return false;
+		}
 
-        schoolForEdit.Name = model.Name;
-        schoolForEdit.Address = model.Address;
-        schoolForEdit.Email = model.Email;
-        schoolForEdit.PhoneNumber = model.PhoneNumber;
-        schoolForEdit.IsActive = model.IsActive;
+		schoolForEdit.Name = model.Name;
+		schoolForEdit.Address = model.Address;
+		schoolForEdit.Email = model.Email;
+		schoolForEdit.PhoneNumber = model.PhoneNumber;
+		schoolForEdit.IsActive = model.IsActive;
 
-        await this._dbContext.SaveChangesAsync();
+		await this._dbContext.SaveChangesAsync();
 
-        return true;
-    }
+		return true;
+	}
 
-    public async Task<SchoolViewModel?> ViewDetailsAsync(string id)
-    {
-        var viewModel = await this._dbContext
-            .Schools
-            .Select(s => new SchoolViewModel
-            {
-                Id = s.Id.ToString(),
-                Name = s.Name,
-                Address = s.Address,
-                Email = s.Email,
-                PhoneNumber = s.PhoneNumber,
-                IsActive = s.IsActive
-            })
-            .AsNoTracking()
-            .FirstOrDefaultAsync(s => s.Id == id);
+	public async Task<SchoolViewModel?> ViewDetailsAsync(string id)
+	{
+		var viewModel = await this._dbContext
+			.Schools
+			.Select(s => new SchoolViewModel
+			{
+				Id = s.Id.ToString(),
+				Name = s.Name,
+				Address = s.Address,
+				Email = s.Email,
+				PhoneNumber = s.PhoneNumber,
+				IsActive = s.IsActive
+			})
+			.AsNoTracking()
+			.FirstOrDefaultAsync(s => s.Id == id);
 
-        return viewModel;
-    }
+		return viewModel;
+	}
 
-    public async Task<bool> DeleteAsync(string? id)
-    {
-        if (string.IsNullOrEmpty(id) == false)
-        {
-            var schoolToDelete = await this._dbContext
-                .Schools
-                .FirstOrDefaultAsync(s => s.Id.ToString() == id);
+	public async Task<bool> DeleteAsync(string? id)
+	{
+		var schoolToDelete = await this._dbContext
+			.Schools
+			.FirstOrDefaultAsync(s => s.Id.ToString() == id);
 
-            if (schoolToDelete != null)
-            {
-                this._dbContext.Schools.Remove(schoolToDelete);
-                await this._dbContext.SaveChangesAsync();
+		if (schoolToDelete != null)
+		{
+			this._dbContext.Schools.Remove(schoolToDelete);
+			await this._dbContext.SaveChangesAsync();
 
-                return true;
-            }
-        }
+			return true;
+		}
+		return false;
+	}
 
-        return false;
-    }
+	public async Task<bool> IsNotNullOrEmptyInputAsync(string id, SchoolViewModel? model)
+	{
+		if (string.IsNullOrEmpty(id))
+		{
+			return false;
+		}
 
-    public async Task<bool> ValidateInputAsync(string id, SchoolViewModel? model)
-    {
-        if (string.IsNullOrEmpty(id))
-        {
-            return false;
-        }
+		if (model != null && string.IsNullOrEmpty(model.Id))
+		{
+			return false;
+		}
 
-        if (model != null && string.IsNullOrEmpty(model.Id))
-        {
-            return false;
-        }
+		return true;
+	}
 
-        return true;
-    }
 }
