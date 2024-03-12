3965fbab1b63955b450c8905076f0ed1f319681b
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-03-12 23:43:50 +0200

    feat: Implement ClassroomService
    Added the ClassroomService class to handle operations related to classrooms. Includes methods to retrieve and manage classroom data asynchronously. Dependencies on KyberKlassDbContext and ISchoolService injected via constructor.

---
+++ KyberKlass.Services.Data/ClassroomService.cs
@@ -0,0 +1,52 @@
+﻿namespace KyberKlass.Services.Data;
+
+using Interfaces;
+using KyberKlass.Data;
+using Microsoft.EntityFrameworkCore;
+using Web.ViewModels.Admin.Classroom;
+
+public class ClassroomService : IClassroomService
+{
+	private readonly KyberKlassDbContext _dbContext;
+	private readonly ISchoolService _schoolService;
+
+	public ClassroomService(KyberKlassDbContext dbContext, ISchoolService schoolService)
+	{
+		this._dbContext = dbContext;
+		this._schoolService = schoolService;
+	}
+
+	public async Task<ManageClassroomsViewModel> GetManageClassroomsAsync(string schoolId)
+	{
+		var school = await this._schoolService.GetByIdAsync(schoolId);
+		IEnumerable<ClassroomViewModel> classrooms = await this.GetClassroomsAsync(schoolId);
+
+		return new ManageClassroomsViewModel
+		{
+			SchoolId = schoolId,
+			SchoolName = school!.Name,
+			Classrooms = classrooms
+				.Select(c => new ClassroomViewModel
+				{
+					Id = c.Id,
+					Name = c.Name
+				})
+		};
+	}
+
+	public async Task<IEnumerable<ClassroomViewModel>> GetClassroomsAsync(string schoolId)
+	{
+		IEnumerable<ClassroomViewModel> classrooms = await this._dbContext
+			.Classrooms
+			.Where(c => c.SchoolId.ToString() == schoolId)
+			.Select(c => new ClassroomViewModel
+			{
+				Id = c.Id.ToString(),
+				Name = c.Name
+			})
+			.AsNoTracking()
+			.ToArrayAsync();
+
+		return classrooms;
+	}
+}
