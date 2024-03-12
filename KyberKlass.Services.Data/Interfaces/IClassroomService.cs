d88d19002b093a5cade374e2c077d1d3b33ef94b
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-03-12 23:43:21 +0200

    feat: Add IClassroomService interface
    Introduced the IClassroomService interface to define methods for managing classrooms within a school. Includes methods to retrieve and manage classroom data asynchronously.

---
+++ KyberKlass.Services.Data/Interfaces/IClassroomService.cs
@@ -0,0 +1,16 @@
+﻿namespace KyberKlass.Services.Data.Interfaces
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using Web.ViewModels.Admin.Classroom;
+
+	public interface IClassroomService
+	{
+		Task<ManageClassroomsViewModel> GetManageClassroomsAsync(string schoolId);
+
+		Task<IEnumerable<ClassroomViewModel>> GetClassroomsAsync(string schoolId);
+	}
+}
