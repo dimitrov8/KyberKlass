a6981b829cdb34664800a8b900ddde2fb7a7aaa0
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-03-13 00:02:24 +0200

    Create ClassroomViewModel

---
+++ KyberKlass.Web.ViewModels/Admin/Classroom/ClassroomViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace KyberKlass.Web.ViewModels.Admin.Classroom;
+
+public class ClassroomViewModel
+{
+    public string Id { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+}
