e14c1efd14d0bc9385e8e3c9671e46da5b5e9a1c
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-03-12 23:45:46 +0200

    feat: Add IStudentService interface with GetUnassignedStudentsAsync method.

---
+++ KyberKlass.Services.Data/Interfaces/IStudentService.cs
@@ -0,0 +1,8 @@
+﻿namespace KyberKlass.Services.Data.Interfaces;
+
+using Web.ViewModels.Admin.User;
+
+public interface IStudentService
+{
+    Task<IEnumerable<UserBasicVIewModel>?> GetUnassignedStudentsAsync();
+}
