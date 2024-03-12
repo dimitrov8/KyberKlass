9b7ebaf385df026087167716530744c24e84470a
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-03-12 23:59:50 +0200

    Add a viewmodel for creating a classroom

---
+++ KyberKlass.Web.ViewModels/Admin/Classroom/ClassroomCreateViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace KyberKlass.Web.ViewModels.Admin.Classroom;
+
+using System.ComponentModel.DataAnnotations;
+using User;
+using static Common.EntityValidations.Classroom;
+
+public class ClassroomCreateViewModel
+{
+    public ClassroomCreateViewModel()
+    {
+        this.Students = new HashSet<UserBasicVIewModel>();
+        this.UnassignedTeachers = new HashSet<UserBasicVIewModel>();
+        this.SelectedStudents = new HashSet<UserBasicVIewModel>();
+    }
+
+    [Required(ErrorMessage = "Classroom name is required.")]
+    [StringLength(MAX_NAME_LENGTH, MinimumLength = MIN_NAME_LENGTH)]
+    public string Name { get; set; } = null!;
+
+    [Required(ErrorMessage = "Please select a teacher")]
+    public string TeacherId { get; set; } = null!;
+
+    [Required]
+    public bool IsActive { get; set; }
+
+    [Required]
+    public string SchoolId { get; set; } = null!;
+
+    public ICollection<UserBasicVIewModel> UnassignedTeachers { get; set; }
+
+    public ICollection<UserBasicVIewModel> Students { get; set; }
+
+    public ICollection<UserBasicVIewModel> SelectedStudents { get; set; }
+}
