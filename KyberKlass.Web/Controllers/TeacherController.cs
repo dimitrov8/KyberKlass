307c8d4ecbf68c8ccb1eaf2d2e6601c675124a69
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-03-12 23:49:56 +0200

    feat: Add TeacherController with All action

---
+++ KyberKlass.Web/Controllers/TeacherController.cs
@@ -0,0 +1,28 @@
+﻿namespace KyberKlass.Web.Controllers
+{
+	using Microsoft.AspNetCore.Mvc;
+	using Services.Data.Interfaces;
+	using ViewModels.Admin.User;
+
+	public class TeacherController : Controller
+	{
+		private readonly ITeacherService _teacherService;
+
+		public TeacherController(ITeacherService teacherService)
+		{
+			this._teacherService = teacherService;
+		}
+
+		private string GetViewPath(string viewName)
+		{
+			return $"~/Views/Admin/Teacher/{viewName}.cshtml";
+		}
+
+		public async Task<IActionResult> All()
+		{
+			List<UserViewModel>? allTeachersViewModel = await this._teacherService.AllAsync();
+
+			return this.View(this.GetViewPath(nameof(this.All)), allTeachersViewModel);
+		}
+	}
+}
