ee2a7708a5e3c9bd4cb5be26e458ab654e6604a7
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-03-13 00:05:15 +0200

    Create UserBasicViewModel

---
+++ KyberKlass.Web.ViewModels/Admin/User/UserBasicViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace KyberKlass.Web.ViewModels.Admin.User;
+
+public class UserBasicViewModel
+{
+    public string Id { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+}
