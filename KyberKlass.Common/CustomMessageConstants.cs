15c740e2242fb5c0cc792be2dbad0a99b008644c
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-03-15 23:18:44 +0200

    Refactor: Improve message consistency and extensibility

--- KyberKlass.Common/CustomMessageConstants.cs
+++ KyberKlass.Common/CustomMessageConstants.cs
@@ -7,20 +7,20 @@ public static class CustomMessageConstants
 		public const string INVALID_INPUT_MESSAGE = "Invalid input. Please check your data and try again.";
 
 		public const string UNABLE_TO_SAVE_CHANGES_MESSAGE = "Something went wrong trying to save the changes you made. Please try again.";
+
+		public const string ALREADY_ADDED_MESSAGE = "{0} \"{1}\" is already added.";
+
+		public const string SUCCESSFULLY_ADDED_MESSAGE = "Successfully added {0} \"{1}\".";
+
+		public const string ADDITION_ERROR_MESSAGE = "An error occurred while adding the {0}.";
+
+		public const string EDIT_ERROR_MESSAGE = "An error occurred while editing the {0}.";
 	}
 
 
 	public static class School
 	{
-		public const string ALREADY_ADDED_MESSAGE = "School \"{0}\" is already added.";
-
-		public const string SUCCESSFULLY_ADDED_MESSAGE = "Successfully added School \"{0}\".";
-
-		public const string ADDITION_ERROR_MESSAGE = "An error occurred while adding the school.";
-
 		public const string SUCCESSFULLY_APPLIED_CHANGES = "Successfully applied changes for School: \"{0}\".";
-
-		public const string EDIT_ERROR_MESSAGE = "An error occurred while editing the school.";
 
 		public const string SUCCESSFULLY_DELETED_MESSAGE = "School successfully deleted.";
 
@@ -34,5 +34,9 @@ public static class CustomMessageConstants
 		public const string ROLE_UPDATE_FAILED_MESSAGE = "Failed to update user role.";
 
 		public const string ROLE_UPDATE_SUCCESS_MESSAGE = "Successfully updated role.";
+
+		public const string SUCCESSFULLY_APPLIED_CHANGES_MESSAGE = "Successfully applied changes for User with Id: \"{0}\".";
+
+		public const string SUCCESSFULLY_SOFT_DELETED_MESSAGE = "Successfully soft deleted User with Id: \"{0}\".";
 	}
 }
