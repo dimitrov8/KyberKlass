80ee8900dc85e747daa4ddba73ee9efa24fa8a35
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-03-17 19:02:02 +0200

    Add FormattingConstants class

---
+++ KyberKlass.Common/FormattingConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace KyberKlass.Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class FormattingConstants
+    {
+        public const string BIRTH_DATE_FORMAT = "yyyy-MM-dd";
+
+    }
+}
