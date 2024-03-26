cb0ba5a4ce142961866b8ca68283db594b6319e2
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-03-26 14:15:56 +0200

    -Introduced GetAllGuardiansAsync -Updated UpdateRoleAsync for flexibility

--- KyberKlass.Services.Data/Interfaces/IUserService.cs
+++ KyberKlass.Services.Data/Interfaces/IUserService.cs
@@ -8,16 +8,18 @@ public interface IUserService
 	Task<List<UserViewModel>> AllAsync();
 
 	Task<UserDetailsViewModel?> GetDetailsAsync(string id);
-
-	Task<UserUpdateRoleViewModel?> GetForUpdateRoleAsync(string id);
-
-	Task<string?> UpdateRoleAsync(string id, string roleId);
-
-	Task<IEnumerable<UserRolesViewModel>> GetAllRolesAsync();
-
-	Task<UserEditFormModel?> GetForEditAsync(string id);
-
-	Task<UserEditFormModel?> EditAsync(string id, UserEditFormModel model);
-
-	Task<ApplicationUser?> GetUserById(string id);
+	
+    Task<UserUpdateRoleViewModel?> GetForUpdateRoleAsync(string id);
+	
+    Task<string?> UpdateRoleAsync(string id, string roleId, string? guardianId, string? classroomId);
+	
+    Task<IEnumerable<UserRolesViewModel>> GetAllRolesAsync();
+	
+    Task<UserEditFormModel?> GetForEditAsync(string id);
+	
+    Task<UserEditFormModel?> EditAsync(string id, UserEditFormModel model);
+	
+    Task<ApplicationUser?> GetUserById(string id);
+    
+    Task<IEnumerable<UserBasicViewModel>> GetAllGuardiansAsync();
 }
