7247c1ab944e897ec6a49e674c6903f261d51857
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-03-26 14:09:47 +0200

    Refactor: Update query filter to use ApplicationUser's IsActive property

--- KyberKlass.Data/Configurations/TeacherEntityConfiguration.cs
+++ KyberKlass.Data/Configurations/TeacherEntityConfiguration.cs
@@ -9,7 +9,7 @@ public class TeacherEntityConfiguration : IEntityTypeConfiguration<Teacher>
 	public void Configure(EntityTypeBuilder<Teacher> builder)
 	{
 		builder
-			.HasQueryFilter(t => t.IsActive);
+			.HasQueryFilter(t => t.ApplicationUser.IsActive);
 
 		builder
 			.HasMany(t => t.Subjects)
