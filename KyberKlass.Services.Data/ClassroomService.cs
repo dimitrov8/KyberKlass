d12970a5133adde66b6162bb3f62ead1d3d06395
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-03-26 14:13:21 +0200

    Enhancement: Update IClassroomService interface to include method for retrieving all school classrooms

--- KyberKlass.Services.Data/ClassroomService.cs
+++ KyberKlass.Services.Data/ClassroomService.cs
@@ -11,120 +11,143 @@ using Web.ViewModels.Admin.Classroom;
 /// </summary>
 public class ClassroomService : IClassroomService
 {
-	private readonly KyberKlassDbContext _dbContext;
-	private readonly ISchoolService _schoolService;
+    private readonly KyberKlassDbContext _dbContext;
+    private readonly ISchoolService _schoolService;
 
-	/// <summary>
-	///     Constructor for ClassroomService.
-	/// </summary>
-	/// <param name="dbContext">The database context.</param>
-	/// <param name="schoolService">The service for managing schools.</param>
-	public ClassroomService(KyberKlassDbContext dbContext, ISchoolService schoolService)
-	{
-		this._dbContext = dbContext;
-		this._schoolService = schoolService;
-	}
+    /// <summary>
+    ///     Constructor for ClassroomService.
+    /// </summary>
+    /// <param name="dbContext">The database context.</param>
+    /// <param name="schoolService">The service for managing schools.</param>
+    public ClassroomService(KyberKlassDbContext dbContext, ISchoolService schoolService)
+    {
+        this._dbContext = dbContext;
+        this._schoolService = schoolService;
+    }
 
-	/// <summary>
-	///     Retrieves a view model containing classrooms for a specified school.
-	/// </summary>
-	/// <param name="schoolId">The ID of the school.</param>
-	/// <returns>A view model containing classrooms for the school.</returns>
-	public async Task<ManageClassroomsViewModel> GetManageClassroomsAsync(string schoolId)
-	{
-		var school = await this._schoolService.GetByIdAsync(schoolId); // Retrieve the school
-		IEnumerable<ClassroomViewModel> classrooms = await this.GetClassroomsAsync(schoolId); // Retrieve classrooms for the school
+    /// <summary>
+    ///     Retrieves a view model containing classrooms for a specified school.
+    /// </summary>
+    /// <param name="schoolId">The ID of the school.</param>
+    /// <returns>A view model containing classrooms for the school.</returns>
+    public async Task<ManageClassroomsViewModel> GetManageClassroomsAsync(string schoolId)
+    {
+        var school = await this._schoolService.GetByIdAsync(schoolId); // Retrieve the school
+        IEnumerable<ClassroomViewModel> classrooms = await this.GetClassroomsAsync(schoolId); // Retrieve classrooms for the school
 
-		return new ManageClassroomsViewModel
-		{
-			SchoolId = schoolId,
-			SchoolName = school!.Name,
-			Classrooms = classrooms
-				.Select(c => new ClassroomViewModel
-				{
-					Id = c.Id,
-					Name = c.Name
-				})
-		}; // Create and return a view model containing classrooms
-	}
+        return new ManageClassroomsViewModel
+        {
+            SchoolId = schoolId,
+            SchoolName = school!.Name,
+            Classrooms = classrooms
+                .Select(c => new ClassroomViewModel
+                {
+                    Id = c.Id,
+                    Name = c.Name
+                })
+        }; // Create and return a view model containing classrooms
+    }
 
-	/// <summary>
-	///     Retrieves classrooms for a specified school.
-	/// </summary>
-	/// <param name="schoolId">The ID of the school.</param>
-	/// <returns>An enumerable collection of classroom view models.</returns>
-	public async Task<IEnumerable<ClassroomViewModel>> GetClassroomsAsync(string schoolId)
-	{
-		IEnumerable<ClassroomViewModel> classrooms = await this._dbContext
-			.Classrooms
-			.Where(c => c.SchoolId == Guid.Parse(schoolId))
-			.Select(c => new ClassroomViewModel
-			{
-				Id = c.Id.ToString(),
-				Name = c.Name
-			})
-			.AsNoTracking()
-			.ToArrayAsync(); // Retrieve classrooms from the database
 
-		return classrooms;
-	}
+    public async Task<IEnumerable<ClassroomViewModel>> GetClassroomsAsync(string schoolId)
+    {
+        Classroom[] classrooms = await this._dbContext
+            .Classrooms
+            .Where(c => c.SchoolId == Guid.Parse(schoolId))
+            .Include(c => c.Students)
+            .Include(u => u.Teacher.ApplicationUser)
+            .AsNoTracking()
+            .ToArrayAsync();
 
-	/// <summary>
-	///     Adds a new classroom to the database.
-	/// </summary>
-	/// <param name="model">The view model containing data for the new classroom.</param>
-	/// <returns>True if the classroom was added successfully; otherwise, false.</returns>
-	public async Task<bool> AddAsync(AddClassroomViewModel model)
-	{
-		var newSchool = await this._schoolService.GetByIdAsync(model.SchoolId); // Retrieve the school by ID
+        IEnumerable<ClassroomViewModel> classroomViewModels = classrooms.Select(c => new ClassroomViewModel
+        {
+            Id = c.Id.ToString(),
+            Name = c.Name,
+            TeacherName = c.Teacher.ApplicationUser.GetFullName(),
+            StudentsCount = c.Students.Count()
+        });
 
-		if (newSchool == null)
-		{
-			return false; // If the school doesn't exist, return false
-		}
+        return classroomViewModels;
+    }
 
-		bool classRoomExists = await this.ClassroomExistsInSchool(model.Name, model.SchoolId); // Check if the classroom already exists in the school
+    public async Task<IEnumerable<ClassroomBasicViewModel>> GetAllSchoolClassroomsAsync()
+    {
+        IEnumerable<ClassroomBasicViewModel> allClassrooms = await this._dbContext
+            .Classrooms
+            .Select(c => new ClassroomBasicViewModel
+            {
+                Id = c.Id.ToString(),
+                Name = c.Name
+            })
+            .AsNoTracking()
+            .ToArrayAsync();
 
-		var schoolIdAsGuid = Guid.Parse(model.SchoolId);
-		var teacherIdAsGuid = Guid.Parse(model.TeacherId);
+        return allClassrooms;
+    }
 
-		var newClassroom = new Classroom
-		{
-			Id = Guid.NewGuid(),
-			Name = model.Name,
-			TeacherId = new Guid(model.TeacherId),
-			SchoolId = schoolIdAsGuid
-		}; // Create a new classroom object
+    /// <summary>
+    ///     Adds a new classroom to the database.
+    /// </summary>
+    /// <param name="model">The view model containing data for the new classroom.</param>
+    /// <returns>True if the classroom was added successfully; otherwise, false.</returns>
+    public async Task<bool> AddAsync(AddClassroomViewModel model)
+    {
+        var newSchool = await this._schoolService.GetByIdAsync(model.SchoolId); // Retrieve the school by ID
 
-		var teacher = await this._dbContext.Teachers.FindAsync(teacherIdAsGuid); // Retrieve the teacher from the database
+        if (newSchool == null)
+        {
+            return false; // If the school doesn't exist, return false
+        }
 
-		if (classRoomExists)
-		{
-			return false; // If the classroom already exists, return false
-		}
+        bool classRoomExists = await this.ClassroomExistsInSchool(model.Name, model.SchoolId); // Check if the classroom already exists in the school
 
-		// TODO
-		var school = await this._dbContext.Schools.FindAsync(schoolIdAsGuid);
+        if (classRoomExists)
+        {
+            return false; // If the classroom already exists, return false
+        }
 
-		// Add the new classroom to the database
-		await this._dbContext.Classrooms.AddAsync(newClassroom);
-		await this._dbContext.SaveChangesAsync();
+        var schoolIdAsGuid = Guid.Parse(model.SchoolId);
+        var teacherIdAsGuid = Guid.Parse(model.TeacherId);
 
-		return true;
-	}
+        var newClassroom = new Classroom
+        {
+            Id = Guid.NewGuid(),
+            Name = model.Name,
+            TeacherId = teacherIdAsGuid,
+            SchoolId = schoolIdAsGuid
+        }; // Create a new classroom object
 
-	/// <summary>
-	///     Checks if a classroom with the given name exists in the specified school.
-	/// </summary>
-	/// <param name="classroomName">The name of the classroom.</param>
-	/// <param name="schoolId">The ID of the school.</param>
-	/// <returns>True if the classroom exists in the school; otherwise, false.</returns>
-	public async Task<bool> ClassroomExistsInSchool(string classroomName, string schoolId)
-	{
-		return await this._dbContext
-			.Classrooms
-			.AsNoTracking()
-			.AnyAsync(c => c.Name == classroomName && c.SchoolId == Guid.Parse(schoolId));
-		// Check if any classroom with the given name and school ID exists in the database
-	}
+        foreach (string studentId in model.SelectedStudents.Select(s => s.Id))
+        {
+            var student = await this._dbContext
+                .Students
+                .FindAsync(Guid.Parse(studentId));
+
+            if (student != null)
+            {
+                newClassroom.Students.Add(student);
+            }
+        }
+
+        // Add the new classroom to the database
+        await this._dbContext.Classrooms.AddAsync(newClassroom);
+        await this._dbContext.SaveChangesAsync();
+
+        return true;
+    }
+
+    /// <summary>
+    ///     Checks if a classroom with the given name exists in the specified school.
+    /// </summary>
+    /// <param name="classroomName">The name of the classroom.</param>
+    /// <param name="schoolId">The ID of the school.</param>
+    /// <returns>True if the classroom exists in the school; otherwise, false.</returns>
+    public async Task<bool> ClassroomExistsInSchool(string classroomName, string schoolId)
+    {
+        return await this._dbContext
+            .Classrooms
+            .AsNoTracking()
+            .AnyAsync(c => c.Name == classroomName && c.SchoolId == Guid.Parse(schoolId));
+        // Check if any classroom with the given name and school ID exists in the database
+    }
 }
