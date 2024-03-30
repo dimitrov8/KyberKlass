d2e86e2fe0504ddcfd01d014ec81b416079ec074
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-03-30 07:38:41 +0200

    Updated GuardianViewModel in the Admin namespace to include an Address property and removed the StudentNames property.

--- KyberKlass.Web.ViewModels/Admin/Guardian/GuardianViewModel.cs
+++ KyberKlass.Web.ViewModels/Admin/Guardian/GuardianViewModel.cs
@@ -2,16 +2,11 @@
 
 public class GuardianViewModel
 {
-	public GuardianViewModel()
-	{
-		this.StudentNames = new HashSet<string>();
-	}
+    public string FullName { get; set; } = null!;
 
-	public string FullName { get; set; } = null!;
+    public string Address { get; set; } = null!;
 
-	public string Email { get; set; } = null!;
+    public string Email { get; set; } = null!;
 
 	public string PhoneNumber { get; set; } = null!;
-
-	public IEnumerable<string> StudentNames { get; set; }
 }
