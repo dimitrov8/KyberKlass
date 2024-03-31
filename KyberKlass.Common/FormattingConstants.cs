543521525cbf15b5f6c558dcf17df3d7a8204232
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-03-31 04:21:08 +0300

    refactor

--- KyberKlass.Common/FormattingConstants.cs
+++ KyberKlass.Common/FormattingConstants.cs
@@ -1,14 +1,6 @@
-﻿namespace KyberKlass.Common
+﻿namespace KyberKlass.Common;
+
+public class FormattingConstants
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
-    public class FormattingConstants
-    {
-        public const string BIRTH_DATE_FORMAT = "yyyy-MM-dd";
-
-    }
+	public const string BIRTH_DATE_FORMAT = "yyyy-MM-dd";
 }
