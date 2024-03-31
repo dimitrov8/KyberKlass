2665f261b912370d94487a33a52dcbbd0dda1207
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-03-31 04:22:26 +0300

    refactor

--- KyberKlass.Web/Controllers/UserController.cs
+++ KyberKlass.Web/Controllers/UserController.cs
@@ -11,221 +11,218 @@ using static Common.CustomMessageConstants.User;
 [Authorize(Roles = "Admin")]
 public class UserController : Controller
 {
-    private readonly IUserService _userService;
-    private const string CONTROLLER_NAME = "User";
+	private readonly IUserService _userService;
+	private const string CONTROLLER_NAME = "User";
 
-    public UserController(IUserService userService)
-    {
-        this._userService = userService;
-    }
+	public UserController(IUserService userService)
+	{
+		this._userService = userService;
+	}
 
-    private string GetViewPath(string viewName)
-    {
-        return $"~/Views/Admin/User/{viewName}.cshtml";
-    }
+	private string GetViewPath(string viewName)
+	{
+		return $"~/Views/Admin/User/{viewName}.cshtml";
+	}
 
-    [HttpGet]
-    public async Task<IActionResult> All()
-    {
-        List<UserViewModel> allUsersViewModel = await this._userService.AllAsync();
+	[HttpGet]
+	public async Task<IActionResult> All()
+	{
+		List<UserViewModel> allUsersViewModel = await this._userService.AllAsync();
 
-        return this.View(this.GetViewPath(nameof(this.All)), allUsersViewModel);
-    }
+		return this.View(this.GetViewPath(nameof(this.All)), allUsersViewModel);
+	}
 
-    [HttpGet]
-    public async Task<IActionResult> Details(string id)
-    {
-        bool isValidInput = await ValidationExtensions.IsNotNullOrEmptyInputAsync<string>(id, null);
+	[HttpGet]
+	public async Task<IActionResult> Details(string id)
+	{
+		bool isValidInput = await ValidationExtensions.IsNotNullOrEmptyInputAsync<string>(id, null);
 
-        if (isValidInput == false)
-        {
-            return this.BadRequest(INVALID_INPUT_MESSAGE);
-        }
+		if (isValidInput == false)
+		{
+			return this.BadRequest(INVALID_INPUT_MESSAGE);
+		}
 
-        try
-        {
-            var userDetailsViewModel = await this._userService.GetDetailsAsync(id);
+		try
+		{
+			var userDetailsViewModel = await this._userService.GetDetailsAsync(id);
 
-            if (userDetailsViewModel == null)
-            {
-                return this.NotFound();
-            }
+			if (userDetailsViewModel == null)
+			{
+				return this.NotFound();
+			}
 
-            return this.View(this.GetViewPath(nameof(this.Details)), userDetailsViewModel);
-        }
-        catch (Exception)
-        {
-            return this.RedirectToAction(nameof(this.All));
-        }
-    }
+			return this.View(this.GetViewPath(nameof(this.Details)), userDetailsViewModel);
+		}
+		catch (Exception)
+		{
+			return this.RedirectToAction(nameof(this.All));
+		}
+	}
 
-    [HttpGet]
-    public async Task<IActionResult> Edit(string id)
-    {
-        bool isValidInput = await ValidationExtensions.IsNotNullOrEmptyInputAsync<string>(id, null);
+	[HttpGet]
+	public async Task<IActionResult> Edit(string id)
+	{
+		bool isValidInput = await ValidationExtensions.IsNotNullOrEmptyInputAsync<string>(id, null);
 
-        if (isValidInput == false)
-        {
-            return this.BadRequest(INVALID_INPUT_MESSAGE);
-        }
+		if (isValidInput == false)
+		{
+			return this.BadRequest(INVALID_INPUT_MESSAGE);
+		}
 
-        try
-        {
-            var userViewModel = await this._userService.GetForEditAsync(id);
+		try
+		{
+			var userViewModel = await this._userService.GetForEditAsync(id);
 
-            if (userViewModel == null)
-            {
-                return this.NotFound();
-            }
+			if (userViewModel == null)
+			{
+				return this.NotFound();
+			}
 
-            return this.View(this.GetViewPath(nameof(this.Edit)), userViewModel);
-        }
-        catch (Exception)
-        {
-            return this.RedirectToAction(nameof(this.All));
-        }
-    }
+			return this.View(this.GetViewPath(nameof(this.Edit)), userViewModel);
+		}
+		catch (Exception)
+		{
+			return this.RedirectToAction(nameof(this.All));
+		}
+	}
 
-    [HttpPost]
-    public async Task<IActionResult> Edit(string id, UserEditFormModel model)
-    {
-        bool isValidInput = await ValidationExtensions.IsNotNullOrEmptyInputAsync<string>(id, null);
+	[HttpPost]
+	public async Task<IActionResult> Edit(string id, UserEditFormModel model)
+	{
+		bool isValidInput = await ValidationExtensions.IsNotNullOrEmptyInputAsync<string>(id, null);
 
-        if (isValidInput == false)
-        {
-            return this.BadRequest(INVALID_INPUT_MESSAGE);
-        }
+		if (isValidInput == false)
+		{
+			return this.BadRequest(INVALID_INPUT_MESSAGE);
+		}
 
-        if (this.ModelState.IsValid == false)
-        {
-            this.TempData["ErrorMessage"] = UNABLE_TO_SAVE_CHANGES_MESSAGE;
+		if (this.ModelState.IsValid == false)
+		{
+			this.TempData["ErrorMessage"] = UNABLE_TO_SAVE_CHANGES_MESSAGE;
 
-            return this.View(this.GetViewPath(nameof(Edit)), model);
-        }
+			return this.View(this.GetViewPath(nameof(Edit)), model);
+		}
 
-        try
-        {
-            var userToEdit = await this._userService.EditAsync(id, model);
+		try
+		{
+			var userToEdit = await this._userService.EditAsync(id, model);
 
-            if (userToEdit != null)
-            {
-                if (model.IsActive == false)
-                {
-                    this.TempData["SuccessDeleteMessage"] = string.Format(SUCCESSFULLY_SOFT_DELETED_MESSAGE, model.Id);
-                }
-                else
-                {
-                    this.TempData["SuccessMessage"] = string.Format(SUCCESSFULLY_APPLIED_CHANGES_MESSAGE, model.Id);
-                }
-            }
+			if (userToEdit != null)
+			{
+				if (model.IsActive == false)
+				{
+					this.TempData["SuccessMessage"] = string.Format(SUCCESSFULLY_SOFT_DELETED_MESSAGE, model.Id);
+				}
+				else
+				{
+					this.TempData["SuccessMessage"] = string.Format(SUCCESSFULLY_APPLIED_CHANGES_MESSAGE, model.Id);
+				}
+			}
 
-            return this.RedirectToAction(nameof(this.All));
-        }
-        catch (Exception)
-        {
-            this.ModelState.AddModelError(string.Empty, string.Format(EDIT_ERROR_MESSAGE, CONTROLLER_NAME.ToLower()));
+			return this.RedirectToAction(nameof(this.All));
+		}
+		catch (Exception)
+		{
+			this.ModelState.AddModelError(string.Empty, string.Format(EDIT_ERROR_MESSAGE, CONTROLLER_NAME.ToLower()));
 
-            return this.View(this.GetViewPath(nameof(Edit)), model);
-        }
-    }
+			return this.View(this.GetViewPath(nameof(Edit)), model);
+		}
+	}
 
-    [HttpGet]
-    public async Task<IActionResult> UpdateRole(string id)
-    {
-        bool isValidInput = await ValidationExtensions.IsNotNullOrEmptyInputAsync<string>(id, null);
+	[HttpGet]
+	public async Task<IActionResult> UpdateRole(string id)
+	{
+		bool isValidInput = await ValidationExtensions.IsNotNullOrEmptyInputAsync<string>(id, null);
 
-        if (isValidInput == false)
-        {
-            return this.BadRequest(INVALID_INPUT_MESSAGE);
-        }
+		if (isValidInput == false)
+		{
+			return this.BadRequest(INVALID_INPUT_MESSAGE);
+		}
 
-        try
-        {
-            var userUpdateRoleViewModel = await this._userService.GetForUpdateRoleAsync(id);
+		try
+		{
+			var userUpdateRoleViewModel = await this._userService.GetForUpdateRoleAsync(id);
 
-            if (userUpdateRoleViewModel == null)
-            {
-                return this.NotFound();
-            }
+			if (userUpdateRoleViewModel == null)
+			{
+				return this.NotFound();
+			}
 
-            return this.View(this.GetViewPath(nameof(this.UpdateRole)), userUpdateRoleViewModel);
-        }
-        catch (Exception)
-        {
-            return this.RedirectToAction(nameof(this.All));
-        }
-    }
+			return this.View(this.GetViewPath(nameof(this.UpdateRole)), userUpdateRoleViewModel);
+		}
+		catch (Exception)
+		{
+			return this.RedirectToAction(nameof(this.All));
+		}
+	}
 
-    [HttpPost]
-    public async Task<IActionResult> UpdateRoleConfirmed(string id, string roleId, string? guardianId, string? classroomId)
-    {
-        bool isValidInput = await ValidationExtensions.IsNotNullOrEmptyInputAsync<string>(id, null) &&
-                            await ValidationExtensions.IsNotNullOrEmptyInputAsync<string>(roleId, null);
+	[HttpPost]
+	public async Task<IActionResult> UpdateRoleConfirmed(string id, string roleId, string? guardianId, string? classroomId)
+	{
+		bool isValidInput = await ValidationExtensions.IsNotNullOrEmptyInputAsync<string>(id, null) &&
+		                    await ValidationExtensions.IsNotNullOrEmptyInputAsync<string>(roleId, null);
 
-        if (isValidInput == false)
-        {
-            return this.BadRequest(INVALID_INPUT_MESSAGE);
-        }
+		if (isValidInput == false)
+		{
+			return this.BadRequest(INVALID_INPUT_MESSAGE);
+		}
 
-        try
-        {
+		try
+		{
+			var userUpdateRoleViewModel = await this._userService.GetForUpdateRoleAsync(id); // Retrieve the user viewmodel
 
-            var userUpdateRoleViewModel = await this._userService.GetForUpdateRoleAsync(id); // Retrieve the user viewmodel
+			string? currentRole = userUpdateRoleViewModel?.PreviousRoleName; // Gets the name of the role before the update
+			string? roleToUpdateTo = await this._userService.GetRoleNameByIdAsync(roleId); // Get the name of the role which we want to update to
 
-            string? currentRole = userUpdateRoleViewModel?.PreviousRoleName; // Gets the name of the role before the update
-            var roleToUpdateTo = await this._userService.GetRoleNameByIdAsync(roleId); // Get the name of the role which we want to update to
+			if (roleToUpdateTo != null && currentRole == "Teacher") // If user wants to update to a valid role and his current role is "Teacher"
+			{
+				// Checks if teacher is assigned to a classroom
+				bool isTeacherAssignedToClassroom = await this._userService.IsTeacherAssignedToClassroomAsync(id);
 
-            if (roleToUpdateTo != null && currentRole == "Teacher") // If user wants to update to a valid role and his current role is "Teacher"
-            {
-                // Checks if teacher is assigned to a classroom
-                var isTeacherAssignedToClassroom = await this._userService.IsTeacherAssignedToClassroomAsync(id);
+				if (isTeacherAssignedToClassroom) // If teacher is assigned
+				{
+					// Display a message to change the classroom teacher
+					this.TempData["ErrorMessage"] = FAILED_TO_UPDATE_TEACHER_TO_OTHER_ROLE_MESSAGE;
+					return this.RedirectToAction(nameof(this.UpdateRole), new { id });
+				}
+			}
+			else if (roleToUpdateTo != null && currentRole == "Guardian")
+			{
+				bool isGuardianAssignedToStudent = await this._userService.IsGuardianAssignedToStudentAsync(id);
 
-                if (isTeacherAssignedToClassroom) // If teacher is assigned
-                {
-                    // Display a message to change the classroom teacher
-                    this.TempData["ErrorMessage"] = FAILED_TO_UPDATE_TEACHER_TO_OTHER_ROLE_MESSAGE;
-                    return this.RedirectToAction(nameof(this.UpdateRole), new { id });
-                }
-            }
-            else if (roleToUpdateTo != null && currentRole == "Guardian")
-            {
-                var isGuardianAssignedToStudent = await this._userService.IsGuardianAssignedToStudentAsync(id);
+				if (isGuardianAssignedToStudent)
+				{
+					this.TempData["ErrorMessage"] = FAILED_TO_UPDATE_GUARDIAN_TO_OTHER_ROLE_MESSAGE;
+					return this.RedirectToAction(nameof(this.UpdateRole), new { id });
+				}
+			}
 
-                if (isGuardianAssignedToStudent)
-                {
-                    this.TempData["ErrorMessage"] = FAILED_TO_UPDATE_GUARDIAN_TO_OTHER_ROLE_MESSAGE;
-                    return this.RedirectToAction(nameof(this.UpdateRole), new { id });
-                }
-            }
+			bool successfulRoleUpdate = await this._userService.UpdateRoleAsync(id, roleId, guardianId, classroomId);
 
-            bool successfulRoleUpdate = await this._userService.UpdateRoleAsync(id, roleId, guardianId, classroomId);
+			if (successfulRoleUpdate)
+			{
+				this.TempData["SuccessMessage"] = ROLE_UPDATE_SUCCESS_MESSAGE;
 
-            if (successfulRoleUpdate)
-            {
-                this.TempData["RoleUpdateSuccessMessage"] = ROLE_UPDATE_SUCCESS_MESSAGE;
+				return this.RedirectToAction(roleToUpdateTo switch
+				{
+					"Teacher" => nameof(this.All),
+					"Student" or "Guardian" or "Admin" => nameof(this.All),
+					_ => nameof(this.All)
+				});
+			}
 
-                //await this.CreateRecordForUser(id, updatedRoleName);
+			return this.BadRequest(ROLE_UPDATE_FAILED_MESSAGE);
+		}
+		catch (Exception)
+		{
+			return this.RedirectToAction(nameof(this.All)); // Can return custom error view
+		}
+	}
 
-                return this.RedirectToAction(roleToUpdateTo switch
-                {
-                    "Teacher" => nameof(this.All),
-                    "Student" or "Guardian" or "Admin" => nameof(this.All),
-                    _ => nameof(this.All)
-                });
-            }
+	public async Task<IActionResult> GetGuardians(string roleId)
+	{
+		IEnumerable<UserBasicViewModel> guardians = await this._userService.GetAllGuardiansAsync();
 
-            return this.BadRequest(ROLE_UPDATE_FAILED_MESSAGE);
-        }
-        catch (Exception)
-        {
-            return this.RedirectToAction(nameof(this.All)); // Can return custom error view
-        }
-    }
-
-    public async Task<IActionResult> GetGuardians(string roleId)
-    {
-        var guardians = await this._userService.GetAllGuardiansAsync();
-
-        return this.Json(guardians);
-    }
+		return this.Json(guardians);
+	}
 }
