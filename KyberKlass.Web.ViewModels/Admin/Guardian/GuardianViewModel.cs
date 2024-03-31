715a20fe38777369999aec0885e3683b396c6004
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-03-31 04:26:48 +0300

    Add property Id and hashset of students

--- KyberKlass.Web.ViewModels/Admin/Guardian/GuardianViewModel.cs
+++ KyberKlass.Web.ViewModels/Admin/Guardian/GuardianViewModel.cs
@@ -1,12 +1,23 @@
 ﻿namespace KyberKlass.Web.ViewModels.Admin.Guardian;
 
+using User;
+
 public class GuardianViewModel
 {
-    public string FullName { get; set; } = null!;
+	public GuardianViewModel()
+	{
+		this.Students = new HashSet<UserBasicViewModel>();
+	}
 
-    public string Address { get; set; } = null!;
+	public string Id { get; set; } = null!;
 
-    public string Email { get; set; } = null!;
+	public string FullName { get; set; } = null!;
+
+	public string Address { get; set; } = null!;
+
+	public string Email { get; set; } = null!;
 
 	public string PhoneNumber { get; set; } = null!;
+
+	public IEnumerable<UserBasicViewModel> Students { get; set; }
 }
