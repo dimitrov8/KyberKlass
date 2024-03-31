b90a7811a6db529ef8ae65fcb52b9ed405186d3c
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-03-31 04:27:07 +0300

    Add StudentChangeGuardianViewModel

---
+++ KyberKlass.Web.ViewModels/Admin/Student/StudentChangeGuardianViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace KyberKlass.Web.ViewModels.Admin.Student;
+
+using User;
+
+public class StudentChangeGuardianViewModel
+{
+	public StudentChangeGuardianViewModel()
+	{
+		this.AvailableGuardians = new HashSet<UserBasicViewModel>();
+	}
+
+	public UserDetailsViewModel? UserDetails { get; set; }
+
+	public IEnumerable<UserBasicViewModel> AvailableGuardians { get; set; }
+}
