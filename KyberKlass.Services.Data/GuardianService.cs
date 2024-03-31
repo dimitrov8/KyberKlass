623c1f24e0ebd09bf919eb0a62dc9579e37fe573
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-03-31 04:15:15 +0300

    Implement GetById in the GuardianService to get guardian by id

---
+++ KyberKlass.Services.Data/GuardianService.cs
@@ -0,0 +1,24 @@
+﻿namespace KyberKlass.Services.Data;
+
+using Interfaces;
+using KyberKlass.Data;
+using KyberKlass.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class GuardianService : IGuardianService
+{
+	private readonly KyberKlassDbContext _dbContext;
+
+	public GuardianService(KyberKlassDbContext dbContext)
+	{
+		this._dbContext = dbContext;
+	}
+
+	public Task<Guardian?> GetById(string id)
+	{
+		return this._dbContext
+			.Guardians
+			.AsNoTracking()
+			.FirstOrDefaultAsync(g => g.Id == Guid.Parse(id));
+	}
+}
---
+++ KyberKlass.Services.Data/Interfaces/IGuardianService.cs
@@ -0,0 +1,8 @@
+﻿namespace KyberKlass.Services.Data.Interfaces;
+
+using KyberKlass.Data.Models;
+
+public interface IGuardianService
+{
+	Task<Guardian?> GetById(string id);
+}
