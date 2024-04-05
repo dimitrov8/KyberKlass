93521d61e26bd20259a3bbc042d120d06c921fbb
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-04-05 11:31:04 +0300

    -Add a property Students which stores students with their id and full name -Add property StudentsCount which gets the count of students collection

--- KyberKlass.Web.ViewModels/Admin/Classroom/ClassroomViewModel.cs
+++ KyberKlass.Web.ViewModels/Admin/Classroom/ClassroomViewModel.cs
@@ -1,12 +1,22 @@
 ﻿namespace KyberKlass.Web.ViewModels.Admin.Classroom;
 
+using System.Runtime.CompilerServices;
+using User;
+
 public class ClassroomViewModel
 {
+	public ClassroomViewModel()
+	{
+		this.Students = new HashSet<UserBasicViewModel>();
+	}
+
 	public string Id { get; set; } = null!;
 
 	public string Name { get; set; } = null!;
 
 	public string TeacherName { get; set; } = null!;
 
-	public int StudentsCount { get; set; }
+	public ICollection<UserBasicViewModel> Students { get; set; }
+
+	public int StudentsCount => this.Students.Count;
 }
