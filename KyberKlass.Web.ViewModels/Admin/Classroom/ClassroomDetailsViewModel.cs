88569eb06346be6338272ed27745f05cd584f661
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-04-12 11:11:44 +0300

    Add ClassroomDetailsViewModel

---
+++ KyberKlass.Web.ViewModels/Admin/Classroom/ClassroomDetailsViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace KyberKlass.Web.ViewModels.Admin.Classroom;
+
+public class ClassroomDetailsViewModel
+{
+    public ClassroomDetailsViewModel()
+    {
+        this.Students = new HashSet<UserBasicViewModel>();
+    }
+
+    public string Id { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string TeacherName { get; set; } = null!;
+
+    public ICollection<UserBasicViewModel> Students { get; set; }
+
+    public int StudentsCount => this.Students.Count;
+}
