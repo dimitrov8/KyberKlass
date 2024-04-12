97fe6da245b1417a400d60aa50d52a694ad0affc
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-04-12 10:49:56 +0300

    Refactor

--- KyberKlass.Services.Data/Interfaces/ITeacherService.cs
+++ KyberKlass.Services.Data/Interfaces/ITeacherService.cs
@@ -1,11 +1,11 @@
 ﻿namespace KyberKlass.Services.Data.Interfaces;
 
-using KyberKlass.Web.ViewModels.Admin;
+using Web.ViewModels.Admin;
 using Web.ViewModels.Admin.User;
 
 public interface ITeacherService
 {
 	Task<List<UserViewModel>?> AllAsync();
 
-	Task<IEnumerable<BasicViewModel>> GetUnassignedTeachersAsync();
+	Task<IEnumerable<UserBasicViewModel>> GetUnassignedTeachersAsync();
 }
