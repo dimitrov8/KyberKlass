a1bb4a701dad2401a0e4e0e16e59a0004da87afd
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-04-12 14:20:30 +0300

    Implement "Details" Action and add documentations

--- KyberKlass.Services.Data/Interfaces/IClassroomService.cs
+++ KyberKlass.Services.Data/Interfaces/IClassroomService.cs
@@ -5,19 +5,49 @@ using Web.ViewModels.Admin.Classroom;
 
 public interface IClassroomService
 {
-    Task<UserBasicViewModel?> GetClassroomAsync(string id);
-
-    Task<ManageClassroomsViewModel> GetManageClassroomsAsync(string schoolId);
-
-    Task<IEnumerable<ClassroomDetailsViewModel>> GetClassroomsAsync(string schoolId);
-
-    Task<IEnumerable<UserBasicViewModel>> GetAllSchoolClassroomsAsync();
-
-    Task<IEnumerable<UserBasicViewModel>> GetAllClassroomsBySchoolId(string schoolId);
-
+    /// <summary>
+    /// Adds a new classroom asynchronously.
+    /// </summary>
+    /// <param name="model">The model containing information about the classroom to add.</param>
+    /// <returns>True if the classroom was added successfully; otherwise, false.</returns>
     Task<bool> AddAsync(AddClassroomViewModel model);
 
+    /// <summary>
+    /// Checks if a classroom with the given name exists in the specified school.
+    /// </summary>
+    /// <param name="classroomName">The name of the classroom.</param>
+    /// <param name="schoolId">The unique identifier of the school.</param>
+    /// <returns>True if the classroom exists in the school; otherwise, false.</returns>
     Task<bool> ClassroomExistsInSchoolAsync(string classroomName, string schoolId);
+
+    /// <summary>
+    /// Retrieves information of a classroom by unique identifier asynchronously.
+    /// </summary>
+    /// <param name="schoolId">The unique identifier of the school.</param>
+    /// <param name="classroomId">The unique identifier of the classroom to get.</param>
+    /// <returns>The information of the classroom if found; otherwise, null.</returns>
+    Task<ClassroomDetailsViewModel?> GetClassroomAsync(string schoolId, string classroomId);
+
+    ///// <summary>
+    ///// Retrieves details of a classroom by unique identifier asynchronously.
+    ///// </summary>
+    ///// <param name="id">The unique identifier of the classroom.</param>
+    ///// <returns>The details of the classroom if found; otherwise, null.</returns>
+    //Task<ClassroomDetailsViewModel?> GetDetailsAsync(string id);
+
+    /// <summary>
+    /// Retrieves information about all classrooms of a given school by unique identifier asynchronously.
+    /// </summary>
+    /// <param name="schoolId">The unique identifier of the school.</param>
+    /// <returns>A collection of classroom view models representing classrooms in a school.</returns>
+    Task<IEnumerable<ClassroomDetailsViewModel>> GetAllClassroomsAsync(string schoolId);
+
+    /// <summary>
+    /// Retrieves details of all classrooms of a given school by unique identifier asynchronously and returns them as JSON.
+    /// </summary>
+    /// <param name="schoolId">The unique identifier of the school.</param>
+    /// <returns>A collection of classroom view models representing classrooms in a school, serialized as JSON.</returns>
+    Task<IEnumerable<BasicViewModel>> GetAllClassroomsBySchoolIdAsJsonAsync(string schoolId);
 
     //Task<bool> ClassroomExistsAsync(string classroomId);
 
