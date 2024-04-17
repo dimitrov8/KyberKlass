d0586b551a44d5f346612774f9978cd28f13c4b3
dimitrov8 <118697977+dimitrov8@users.noreply.github.com>
2024-04-17 21:50:25 +0300

    Create EndpointConfigurationExtension

---
+++ KyberKlass.Web.Infrastructure/Extensions/EndpointConfigurationExtension.cs
@@ -0,0 +1,49 @@
+﻿namespace KyberKlass.Web.Infrastructure.Extensions;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+public class EndpointConfigurationExtension
+{
+	public static void ConfigureEndpoints(IEndpointRouteBuilder endpoints)
+	{
+		endpoints.MapControllerRoute(
+			"adminSchool",
+			"Admin/School/{action=Index}",
+			new { controller = "School" }
+		);
+
+		endpoints.MapControllerRoute(
+			"adminClassroom",
+			"Admin/Classroom/{action=Index}",
+			new { controller = "Classroom" }
+		);
+
+		endpoints.MapControllerRoute(
+			"adminUser",
+			"Admin/User/{action=Index}",
+			new { controller = "User" }
+		);
+
+		endpoints.MapControllerRoute(
+			"adminTeacher",
+			"Admin/Teacher/{action=Index}",
+			new { controller = "Teacher" }
+		);
+
+		endpoints.MapControllerRoute(
+			"adminStudent",
+			"Admin/Student/{action=Index}",
+			new { controller = "Student" }
+		);
+
+		endpoints.MapControllerRoute(
+			"default",
+			"{controller=Home}/{action=Index}/{id?}");
+	}
+
+	public static void ConfigureRazorPages(IEndpointRouteBuilder endpoints)
+	{
+		endpoints.MapRazorPages();
+	}
+}
