1a869793d26649ebe7697c0dab918dacce900e98
dimitrov8 <lightonokami@gmail.com>
2025-04-05 01:52:27 +0300

    feat: add default admin data including password, role, and personal details

--- KyberKlass.Data/Configurations/Constants/SeedDataConstants.cs
+++ KyberKlass.Data/Configurations/Constants/SeedDataConstants.cs
@@ -6,7 +6,17 @@ public static class SeedDataConstants
     {
         public static readonly Guid DefaultAdminUserId = new("e321fa43-9c90-4e01-8f0a-002eae899e98");
         public const string DEFAULT_ADMIN_EMAIL = "admin@kyberklass.com";
+        public const string DEFAULT_ADMIN_PASSWORD = "admin";
         public static readonly Guid AdminRoleId = new("420abb62-30a5-4983-835e-fe0a46b6f463");
+        public const string DEFAULT_ADMIN_ROLE_NAME = "Admin";
+        public const string DEFAULT_ADMIN_FIRST_NAME = "Admin";
+        public const string DEFAULT_ADMIN_LAST_NAME = "User";
+        public const string DEFAULT_ADMIN_BIRTH_DATE = "01/01/2001";
+        public const string DEFAULT_ADMIN_ADDRESS = "1416 Ryan Mountains";
+        public const string DEFAULT_ADMIN_PUBLIC_STAMP = "0DB9D047-3375-4739-9C32-217CC8337032";
+        public const string DEFAULT_ADMIN_CONCURRENCYSTAMP = "d4f6406d-9b51-4290-994d-cf1bb9668b5e";
+        public const string DEFAULT_ADMIN_PHONE_NUMBER = "08888888888";
+
     }
 
     public static class Teacher
